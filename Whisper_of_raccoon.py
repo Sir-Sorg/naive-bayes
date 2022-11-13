2f3c314c1ef233b24a727b96a2c310879490fdc8
Sir-Sorg <sinaorojlo53@gmail.com>
2022-11-13 22:33:07 +0330

    trainig set and test set created

--- Whisper_of_raccoon.py
+++ Whisper_of_raccoon.py
@@ -14,8 +14,14 @@ def cleanData(data):
     return list(filter(lambda thisList: False if ' ?' in thisList else True, data))
 
 
+# Education     Status     Others      Skin Color     Sex
 fileAddres = './adult.csv'
 data = readData(fileAddres)
 print(len(data))
 data = cleanData(data)
 print(len(data))
+
+# learn percent = 70% and test = 30% --> lentgh * 0.3 and lentgh * 0.7 !!!
+trainTestBorder = round(len(data)*0.7)
+trainingData = data[1:trainTestBorder]
+testingData = data[trainTestBorder:]
